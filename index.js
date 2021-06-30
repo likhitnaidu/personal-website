47f05df1b3c32a96f9430d07506299d410bf0575
M.Likhit Naidu <71972560+likhitnaidu@users.noreply.github.com>
2021-06-30 20:46:05 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const options = {
+    bottom: '1000px', // default: '32px'
+    right: '10px', // default: '32px'
+    left: '900px', // default: 'unset'
+    top: '64px',const options = {
+    bottom: '1000px', // default: '32px'
+    right: '10px', // default: '32px'
+    left: '900px', // default: 'unset'
+    top: '64px',
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: 'dark mode', // default: ''
+    autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: 'dark mode', // default: ''
+    autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
